35da1a99b3a1e736aca31a7b10586397cb0f7fee
sahrulabadi <102948347+sahrulabadi@users.noreply.github.com>
2022-04-04 16:12:29 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhamad Sahrul Abadi
+        Saya tinggal didaerah Sunter
+        olahraga yang saya sukai adalah
+        Futsal";
+    }
+}
